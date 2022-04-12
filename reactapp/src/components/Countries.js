acd3c0448049777cc8a4162a3d63aca2aefa32b2
sulecakir <84274212+sulecakir@users.noreply.github.com>
2022-04-12 06:27:12 +0300

    Bootstrap Table Created
    Bootstrap Table Created

--- reactapp/src/components/Countries.js
+++ reactapp/src/components/Countries.js
@@ -1,16 +1,52 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import * as ReactBootStrap from 'react-bootstrap'
 
-class Countries extends Component {
-  render() {
-    return (
-      <div>
-        <table class="table table-striped table-hover">
-  <td> Country</td>
-  <td> Flag</td>
-</table>
-      </div>
-    )
-  }
+
+const Countries = () => {
+
+
+
+const [posts, setPosts] = useState({blogs: []})
+
+useEffect(() => {
+    const fetchPostList = async () => {
+        const {data} = await axios("https://restcountries.com/v2/all")
+
+        setPosts({blogs: data})
+    }
+    fetchPostList()
+}, [setPosts])
+
+return(
+
+    <div>
+        <ReactBootStrap.Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Capital</th>
+      <th>Region</th>
+      <th>Flag</th>
+    </tr>
+  </thead>
+  <tbody>
+
+    {
+        posts.blogs && posts.blogs.map((item) =>(
+            <tr key={item.name}>
+             <td>{item.name}</td>
+             <td>{item.capital}</td>
+             <td>{item.region}</td>
+             <img src={item.flag} width={100} ></img>
+         </tr>
+        ))
+    }
+    
+  </tbody>
+</ReactBootStrap.Table>
+    </div>
+
+)
 }
-
 export default Countries;
